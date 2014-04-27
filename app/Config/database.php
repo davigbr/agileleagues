b4aa741ea5c83ac675db93df12a329b1c722e755
davigbr <davi.gbr@gmail.com>
2014-04-27 11:04:05 -0300

    Database.php modified

--- app/Config/database.php
+++ app/Config/database.php
@@ -64,7 +64,7 @@ class DATABASE_CONFIG {
 	public $default = array(
 		'datasource' => 'Database/Mysql',
 		'persistent' => false,
-		'host' => '127.5.136.130',
+		'host' => 'localhost',
 		'login' => 'adminnEmhFH2',
 		'password' => 'HBwXzDKIWvIF',
 		'database' => 'agileleagues',
