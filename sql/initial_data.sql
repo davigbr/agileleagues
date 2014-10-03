b7632a00c9c99fc68930027224a740fc1ef52ca5
Davi Gabriel da Silva <davi.gbr@gmail.com>
2014-10-03 08:24:53 -0300

    Added initial_data.sql script

---
+++ sql/initial_data.sql
@@ -0,0 +1,14 @@
+INSERT INTO player_type VALUES
+(1, 'Player'),
+(2, 'Game Administrator');
+
+INSERT INTO title VALUES 
+(1, 'Unfleshed',    0),
+(2, 'Novice',       5),
+(3, 'Apprentice',  10),
+(4, 'Sorcerer',    20),
+(5, 'Wizard',      30),
+(6, 'Magician',    40),
+(7, 'Archmage',    50),
+(8, 'Maester',     60),
+(9, 'Archmaester', 70);
